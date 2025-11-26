aa0042482588588b36560abf65e6324806479775
Victor Morales <victor1507morales@gmail.com>
2025-11-26 00:27:59 -0300

    agrego hover en el menu de ajustes

--- UndergroundRaces/UndergroundRaces/EscenaMenuAjustes.cs
+++ UndergroundRaces/UndergroundRaces/EscenaMenuAjustes.cs
@@ -12,15 +12,21 @@ namespace UndergroundRaces
 {
     public class EscenaMenuAjustes : IEscena
     {
-        private Texture2D _fondoAjustes;
+        private Texture2D _fondoAtlas;
+        private List<Rectangle> _framesAjustes = new();
+        private int _frameActual = 9; // 0–8 = hover botones, 9 = normal
+        private int _atlasCols = 2;
+        private int _atlasRows = 5;
+        private int[] _hoverToFrame = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 1, 0 };
+        private int _lastHovered = -1;
+
         private GraphicsDevice _graphicsDevice;
         private ContentManager _content;
-        private Rectangle _botonAtras;
+        private Texture2D _debugPixel;
         private MouseState _mouse;
         private MouseState _prevMouse;
-        private Texture2D _debugPixel;
 
-        // UI rects
+        private Rectangle _botonAtras;
         private Rectangle _sfxMinus, _sfxPlus, _musicMinus, _musicPlus, _masterMinus, _masterPlus, _brightMinus, _brightPlus;
         private Rectangle _sfxBar, _musicBar, _masterBar, _brightBar;
 
@@ -31,7 +37,19 @@ namespace UndergroundRaces
             _graphicsDevice = game.GraphicsDevice;
             _content = game.Content;
 
-            _fondoAjustes = _content.Load<Texture2D>("images/menu-ajustes-underground-races-2025");
+            _fondoAtlas = _content.Load<Texture2D>("images/menu-ajustes-plantillas-underground-races-2025");
+
+            int frameW = _fondoAtlas.Width / _atlasCols;
+            int frameH = _fondoAtlas.Height / _atlasRows;
+            _framesAjustes.Clear();
+            for (int r = 0; r < _atlasRows; r++)
+            {
+                for (int c = 0; c < _atlasCols; c++)
+                {
+                    _framesAjustes.Add(new Rectangle(c * frameW, r * frameH, frameW, frameH));
+                }
+            }
+
             _botonAtras = new Rectangle(20, 20, 60, 60);
 
             _debugPixel = new Texture2D(_graphicsDevice, 1, 1);
@@ -40,7 +58,6 @@ namespace UndergroundRaces
             int minusW = 36;
             int plusW = 36;
 
-            // Coordenadas de botones +/- por fila
             _sfxMinus    = new Rectangle(266 - minusW / 2, 141 - 14, minusW, 28);
             _sfxPlus     = new Rectangle(642 - plusW / 2, 141 - 14, plusW, 28);
             _musicMinus  = new Rectangle(389 - minusW / 2, 267 - 14, minusW, 28);
@@ -50,7 +67,6 @@ namespace UndergroundRaces
             _brightMinus = new Rectangle(378 - minusW / 2, 514 - 14, minusW, 28);
             _brightPlus  = new Rectangle(763 - plusW / 2, 514 - 14, plusW, 28);
 
-            // Centrar barras entre botones
             int barraW = 160;
             int barraH = 28;
             int barraYoffset = 0;
@@ -65,11 +81,22 @@ namespace UndergroundRaces
         {
             _mouse = Mouse.GetState();
 
-            if (_botonAtras.Contains(_mouse.Position) &&
-                _mouse.LeftButton == ButtonState.Pressed &&
-                _prevMouse.LeftButton == ButtonState.Released)
+            int hovered = 9; // default: normal
+
+            if (_sfxMinus.Contains(_mouse.Position)) hovered = 0;
+            else if (_sfxPlus.Contains(_mouse.Position)) hovered = 1;
+            else if (_musicMinus.Contains(_mouse.Position)) hovered = 2;
+            else if (_musicPlus.Contains(_mouse.Position)) hovered = 3;
+            else if (_masterMinus.Contains(_mouse.Position)) hovered = 4;
+            else if (_masterPlus.Contains(_mouse.Position)) hovered = 5;
+            else if (_brightMinus.Contains(_mouse.Position)) hovered = 6;
+            else if (_brightPlus.Contains(_mouse.Position)) hovered = 7;
+            else if (_botonAtras.Contains(_mouse.Position)) hovered = 8;
+
+            if (hovered != _lastHovered)
             {
-                OnVolverClick?.Invoke();
+                _frameActual = hovered;
+                _lastHovered = hovered;
             }
 
             if (_mouse.LeftButton == ButtonState.Pressed &&
@@ -87,6 +114,8 @@ namespace UndergroundRaces
                 if (_brightMinus.Contains(_mouse.Position)) Settings.DecreaseBrightness();
                 else if (_brightPlus.Contains(_mouse.Position)) Settings.IncreaseBrightness();
 
+                if (_botonAtras.Contains(_mouse.Position)) OnVolverClick?.Invoke();
+
                 MediaPlayer.Volume = Settings.MusicVolume * Settings.MasterVolume;
             }
 
@@ -97,7 +126,12 @@ namespace UndergroundRaces
         {
             spriteBatch.Begin();
 
-            spriteBatch.Draw(_fondoAjustes, new Rectangle(0, 0, 1024, 576), Color.White);
+            if (_framesAjustes.Count == (_atlasCols * _atlasRows) && _fondoAtlas != null)
+            {
+                int frameIndex = _hoverToFrame[MathHelper.Clamp(_frameActual, 0, _hoverToFrame.Length - 1)];
+                Rectangle src = _framesAjustes[MathHelper.Clamp(frameIndex, 0, _framesAjustes.Count - 1)];
+                spriteBatch.Draw(_fondoAtlas, new Rectangle(0, 0, 1024, 576), src, Color.White);
+            }
 
             float overlayAlpha = 1f - Settings.Brightness;
             if (_debugPixel != null && overlayAlpha > 0f)
--- UndergroundRaces/UndergroundRaces/obj/Debug/net9.0/UndergroundRaces.AssemblyInfo.cs
+++ UndergroundRaces/UndergroundRaces/obj/Debug/net9.0/UndergroundRaces.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UndergroundRaces")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+addeff581b107e4db4b842821d40d2101b20826d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff32d1bf8c88277ada1dd7954b57b241d56eb72a")]
 [assembly: System.Reflection.AssemblyProductAttribute("UndergroundRaces")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UndergroundRaces")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
